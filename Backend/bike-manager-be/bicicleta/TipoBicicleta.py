ef5894022114ab0416b5ce03d3efb3a59fb8079d
Tiago Ceccon <ceccon.t@hotmail.com>
2021-10-13 10:49:00 -0300

    Cria endpoint no backend para bicicletas

---
+++ Backend/bike-manager-be/bicicleta/TipoBicicleta.py
@@ -0,0 +1,6 @@
+from enum import Enum 
+
+class TipoBicicleta(Enum):
+    urbana = 'urbana'
+    transporte = 'transporte'
+    corrida = 'corrida'
--- Backend/bike-manager-be/main.py
+++ Backend/bike-manager-be/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI 
 
+from bicicleta.TipoBicicleta import TipoBicicleta
+
 app = FastAPI() 
 
 @app.get("/")
 def server_status_index():
     return {"Status": "It lives!"}
+
+@app.get("/bicicletas")
+def get_bicicletas(tipo: TipoBicicleta = None, marchas: int = None, aro: int = None):
+    print(f'Searched for tipo: {tipo}, marchas: {marchas}, aro: {aro}')
+    return []
