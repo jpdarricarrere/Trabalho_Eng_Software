ec9fbfd22d57a87f3f5159eca5aa6055021db12a
Tiago Ceccon <ceccon.t@hotmail.com>
2021-10-13 10:51:10 -0300

    Refatora codigo para usar termo bike em vez de bicicleta

--- Backend/bike-manager-be/bike/TipoBike.py
+++ Backend/bike-manager-be/bike/TipoBike.py
@@ -1,6 +1,6 @@
 from enum import Enum 
 
-class TipoBicicleta(Enum):
+class TipoBike(Enum):
     urbana = 'urbana'
     transporte = 'transporte'
     corrida = 'corrida'
--- Backend/bike-manager-be/main.py
+++ Backend/bike-manager-be/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI 
 
-from bicicleta.TipoBicicleta import TipoBicicleta
+from bike.TipoBike import TipoBike
 
 app = FastAPI() 
 
@@ -8,7 +8,7 @@ app = FastAPI()
 def server_status_index():
     return {"Status": "It lives!"}
 
-@app.get("/bicicletas")
-def get_bicicletas(tipo: TipoBicicleta = None, marchas: int = None, aro: int = None):
+@app.get("/bikes")
+def get_bikes(tipo: TipoBike = None, marchas: int = None, aro: int = None):
     print(f'Searched for tipo: {tipo}, marchas: {marchas}, aro: {aro}')
     return []
