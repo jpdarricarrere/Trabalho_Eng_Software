0df38de37f4e4c03e95e502597f5c80f030ed8fc
gdsantana <guilherme30santana@gmail.com>
2021-10-14 17:11:48 -0300

    update

--- Frontend/bicicleta/src/App.vue
+++ Frontend/bicicleta/src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="left">
-    <img  alt="Vue logo" src="./assets/logo-bike.png">
+  <div>
+    <img alt="vue" src="./assets/logo-bike.png" />
+
+ 
+    <input class="form-control" v-model="search" placeholder="Procure por nome ou modelo de bicicleta">
+      <div>
+  <b-card
+    title="Card Title"
+    img-src="/assets/bike-mtb.png"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+
+
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
-  
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  //  HelloWorld
-  }
-}
+    //  HelloWorld
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#left{
-  left: 0px;
-}
+
 </style>
