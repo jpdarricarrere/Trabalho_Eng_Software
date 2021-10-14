87c73c837d1d4ae68db9270151cab461fba597ca
gdsantana <guilherme30santana@gmail.com>
2021-10-14 16:24:59 -0300

    mudando pra o vue 2

--- Frontend/bicicleta/README.md
+++ Frontend/bicicleta/README.md
@@ -1,4 +1,4 @@
-# bike-manager
+# bicicleta
 
 ## Project setup
 ```
---
+++ Frontend/bicicleta/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="left">
+    <img  alt="Vue logo" src="./assets/logo-bike.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+  
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+  //  HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#left{
+  left: 0px;
+}
+</style>
