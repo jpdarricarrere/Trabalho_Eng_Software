d65f48c3494f4ce13e1f5f5564374bd82cb0c8fc
gdsantana <guilherme30santana@gmail.com>
2021-10-14 14:28:53 -0300

    ajeitando a casa

--- Frontend/bike-manager/src/views/Home.vue
+++ Frontend/bike-manager/src/views/Home.vue
@@ -4,7 +4,7 @@
     <div class="container">
     <div class="row">
       <div class="col">
-        <div class="card-body mt-2" >
+        <div class="card-body" >
           <img src="../assets/bike-mtb.png" />
         </div>
       </div>
