8e45ff46cbcda82939cc304d1f1fb67c70793abe
gdsantana <guilherme30santana@gmail.com>
2021-10-14 14:44:44 -0300

    add package.json to gitignore

--- Frontend/bike-manager/src/views/Home.vue
+++ Frontend/bike-manager/src/views/Home.vue
@@ -1,34 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo-bike.png">
-    <div class="container">
+    <img alt="Vue logo" src="../assets/logo-bike.png" />
+
+    <h1>Alguel de Bicicletas</h1>
+  </div>
+  <!--
+    
+<div class="container px-4 px-lg-5">
     <div class="row">
       <div class="col">
-        <div class="card-body" >
-          <img src="../assets/bike-mtb.png" />
+        <div class="card-body " >
+          <img src= />
         </div>
       </div>
     </div>
-
+    -->
+  <section class="py-5">
+    <div
+      class="
+        row
+        gx-4 gx-lg-5
+        row-cols-2 row-cols-md-3 row-cols-xl-4
+        justify-content-center
+      "
+    >
+      <div class="col mb-5">
+        <div class="card-body h-100">
+          <!-- Product image-->
+          <img class="card-img-top" src="../assets/bike-mtb.png" alt="..." />
+          <!-- Product details-->
+          <div class="card-body p-4">
+            <div class="text-center">
+              <!-- Product name-->
+              <h5 class="fw-bolder">Fancy Product</h5>
+              <!-- Product price-->
+              $40.00 - $80.00
+            </div>
+          </div>
+          <!-- Product actions-->
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">View options</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-   <h1>Alguel de Bicicletas</h1>
-
-  </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
 <style scoped>
-#bike{
+#bike {
   max-width: 100px;
   max-height: 100px;
 }
