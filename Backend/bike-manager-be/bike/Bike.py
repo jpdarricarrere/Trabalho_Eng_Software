30f00634201148ae8547c955ed836f849045ec36
Tiago Ceccon <ceccon.t@hotmail.com>
2021-10-15 18:55:51 -0300

    Backend - Adiciona propriedades nome, modelo e link_imagem em bicicletas

--- Backend/bike-manager-be/bike/Bike.py
+++ Backend/bike-manager-be/bike/Bike.py
@@ -2,8 +2,11 @@ from .TipoBike import TipoBike
 
 class Bike:
 
-    def __init__(self, id: int, alugada: bool, em_manutencao: bool, tipo: TipoBike, num_marchas: int, ano: int, aro: int, id_adm: int):
+    def __init__(self, id: int, nome: str, modelo: str, link_imagem: str, alugada: bool, em_manutencao: bool, tipo: TipoBike, num_marchas: int, ano: int, aro: int, id_adm: int):
         self._id = id
+        self._nome = nome 
+        self._modelo = modelo 
+        self._link_imagem = link_imagem
         self._alugada = alugada
         self._em_manutencao = em_manutencao
         self._tipo = tipo 
@@ -17,6 +20,24 @@ class Bike:
 
     def get_id(self):
         return self._id 
+
+    def set_nome(self, nome: str):
+        self._nome = nome 
+
+    def get_nome(self):
+        return self._nome
+
+    def set_modelo(self, modelo: str):
+        self._modelo = modelo
+
+    def get_modelo(self):
+        return self._modelo
+
+    def set_link_image(self, link_imagem: str):
+        self._link_imagem = link_imagem 
+
+    def get_link_imagem(self):
+        return self._link_imagem
 
     def set_alugada(self, alugada: bool):
         self._alugada = alugada
--- Backend/bike-manager-be/bike/persistencia/MockRepositorioBike.py
+++ Backend/bike-manager-be/bike/persistencia/MockRepositorioBike.py
@@ -3,16 +3,16 @@ from ..TipoBike import TipoBike
 from .IRepositorioBike import IRepositorioBike
 
 _all_bikes = [
-    Bike(id=1, alugada=False, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=5, ano=2020, aro=7, id_adm=1),
-    Bike(id=2, alugada=False, em_manutencao=True, tipo=TipoBike.transporte, num_marchas=4, ano=2017, aro=6, id_adm=1),
-    Bike(id=3, alugada=True, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=6, ano=2021, aro=5, id_adm=1),
-    Bike(id=4, alugada=False, em_manutencao=False, tipo=TipoBike.transporte, num_marchas=6, ano=2019, aro=8, id_adm=1),
-    Bike(id=5, alugada=True, em_manutencao=False, tipo=TipoBike.transporte, num_marchas=5, ano=2017, aro=7, id_adm=1),
-    Bike(id=6, alugada=False, em_manutencao=True, tipo=TipoBike.corrida, num_marchas=5, ano=2019, aro=7, id_adm=1),
-    Bike(id=7, alugada=False, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=6, ano=2018, aro=6, id_adm=1),
-    Bike(id=8, alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=4, ano=2018, aro=6, id_adm=1),
-    Bike(id=9, alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=4, ano=2020, aro=7, id_adm=1),
-    Bike(id=10, alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=5, ano=2021, aro=5, id_adm=1),
+    Bike(id=1, nome="Bicicleta 1", modelo="Modelo1", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=False, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=5, ano=2020, aro=7, id_adm=1),
+    Bike(id=2, nome="Bicicleta 2", modelo="Modelo2", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=False, em_manutencao=True, tipo=TipoBike.transporte, num_marchas=4, ano=2017, aro=6, id_adm=1),
+    Bike(id=3, nome="Bicicleta 3", modelo="Modelo3", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=True, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=6, ano=2021, aro=5, id_adm=1),
+    Bike(id=4, nome="Bicicleta 4", modelo="Modelo4", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=False, em_manutencao=False, tipo=TipoBike.transporte, num_marchas=6, ano=2019, aro=8, id_adm=1),
+    Bike(id=5, nome="Bicicleta 5", modelo="Modelo5", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=True, em_manutencao=False, tipo=TipoBike.transporte, num_marchas=5, ano=2017, aro=7, id_adm=1),
+    Bike(id=6, nome="Bicicleta 6", modelo="Modelo6", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=False, em_manutencao=True, tipo=TipoBike.corrida, num_marchas=5, ano=2019, aro=7, id_adm=1),
+    Bike(id=7, nome="Bicicleta 7", modelo="Modelo7", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=False, em_manutencao=False, tipo=TipoBike.urbana, num_marchas=6, ano=2018, aro=6, id_adm=1),
+    Bike(id=8, nome="Bicicleta 8", modelo="Modelo8", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=4, ano=2018, aro=6, id_adm=1),
+    Bike(id=9, nome="Bicicleta 9", modelo="Modelo9", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=4, ano=2020, aro=7, id_adm=1),
+    Bike(id=10, nome="Bicicleta 10", modelo="Modelo10", link_imagem="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg", alugada=True, em_manutencao=False, tipo=TipoBike.corrida, num_marchas=5, ano=2021, aro=5, id_adm=1),
 ]
 
 class MockRepositorioBike(IRepositorioBike):
