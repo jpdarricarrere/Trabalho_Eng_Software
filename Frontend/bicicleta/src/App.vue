2fae32b382d843e6a4dad25de936a23ee3bd3c50
lleoreis <38788369+lleoreis@users.noreply.github.com>
2021-10-14 21:51:56 -0300

    cards das bicicletas

--- Frontend/bicicleta/src/App.vue
+++ Frontend/bicicleta/src/App.vue
@@ -1,19 +1,21 @@
 <template>
+  <div >
+    <div id="header">
+      <img id="logo" alt="logo" src="./assets/logo-bike.png" />
+      <input class="input-search" v-model="search" placeholder="Procure por nome ou modelo de bicicleta">
+    </div>
+    
   <div>
-    <img alt="vue" src="./assets/logo-bike.png" />
-
- 
-    <input class="form-control" v-model="search" placeholder="Procure por nome ou modelo de bicicleta">
-      <div>
         <!-- img-src so pega imagens com url -->
-        
+  <div class="col d-flex justify-content-center ">
+  <b-card-group> 
   <b-card
     title= "MTB Specialized"
     img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
     img-alt="Image"
     img-top
     tag="article"
-    style="max-width: 20rem;"
+    style="max-width: 19.5rem;"
     class="mb-2"
   >
     <b-card-text>
@@ -22,6 +24,87 @@
 
     <b-button href="javascript:;" variant="primary">Alugue</b-button>
   </b-card>
+  <b-card
+    title= "MTB Specialized"
+    img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19.5rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Bicicleta Aro 29 boa para trilhas em terrenos dificeis 
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Alugue</b-button>
+  </b-card>
+  <b-card
+    title= "MTB Specialized"
+    img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19.5rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Bicicleta Aro 29 boa para trilhas em terrenos dificeis 
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Alugue</b-button>
+  </b-card>
+  </b-card-group> 
+  </div>
+  <div class="col d-flex justify-content-center">
+  <b-card-group> 
+  <b-card
+    title= "MTB Specialized"
+    img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19.5rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Bicicleta Aro 29 boa para trilhas em terrenos dificeis 
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Alugue</b-button>
+  </b-card>
+  <b-card
+    title= "MTB Specialized"
+    img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19.5rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Bicicleta Aro 29 boa para trilhas em terrenos dificeis 
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Alugue</b-button>
+  </b-card>
+  <b-card
+    title= "MTB Specialized"
+    img-src="https://d2ul2exfru69gk.cloudfront.net/Custom/Content/Products/13/16/13163_bicicleta-nova-specialized-epic-ht-carbon-29-2020-37241_z4_637288976190418780.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 19.5rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Bicicleta Aro 29 boa para trilhas em terrenos dificeis 
+    </b-card-text>
+
+    <b-button href="javascript:;" variant="primary">Alugue</b-button>
+  </b-card>
+  </b-card-group> 
+  </div>
 </div>
 
 
@@ -41,5 +124,20 @@ export default {
 </script>
 
 <style>
+#logo{
+  max-width: 80px;
+  max-height: 80px;
+}
+
+.input-search{
+  height: 50%;
+  width: 30%;
+  margin-left: 30%;
+}
+
+.mb-2{
+    margin-left: 10px;
+}
+
 
 </style>
