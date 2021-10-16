1b7a043f03416a683b5b4ec9ac45a0d0a418f748
gdsantana <guilherme30santana@gmail.com>
2021-10-16 15:30:50 -0300

    Fix CORS error

--- Backend/bike-manager-be/main.py
+++ Backend/bike-manager-be/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI 
-
+from fastapi.middleware.cors import CORSMiddleware
 from bike.TipoBike import TipoBike
 from bike.persistencia.MockRepositorioBike import MockRepositorioBike as RepositorioBike
 
 app = FastAPI() 
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def server_status_index():
     return {"Status": "It lives!"}
