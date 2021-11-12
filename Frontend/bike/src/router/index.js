7563a2b697896714886519192c4d0f475691446a
gdsantana <guilherme30santana@gmail.com>
2021-11-12 14:46:29 -0300

    ajeitando umas coisas

--- Frontend/bike/src/router/index.js
+++ Frontend/bike/src/router/index.js
@@ -39,6 +39,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "about" */ '../views/CreateUser.vue')
+    },
+    {
+        path: '/payment',
+        name: 'Payment',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Payment.vue')
     }
 
 ]
