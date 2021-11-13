7eef677c86b5d82e65928ca0cfee44d083345c16
Tiago Ceccon <ceccon.t@hotmail.com>
2021-11-13 12:11:25 -0300

    BE - Refatora entidade Usuario para nao usar properties

--- Backend/bike-manager-be/usuario/Usuario.py
+++ Backend/bike-manager-be/usuario/Usuario.py
@@ -4,57 +4,45 @@ from .TipoUsuario import TipoUsuario
 class Usuario:
 
     def __init__(self, id: int, tipo: TipoUsuario, nome: str, data_nascimento: datetime, email: str, senha: str):
-        self._id = id
-        self._tipo = tipo
-        self._nome = nome
-        self._data_nascimento = data_nascimento
-        self._email = email
-        self._senha = senha
+        self.set_id(id)
+        self.set_tipo(tipo)
+        self.set_nome(nome)
+        self.set_data_nascimento(data_nascimento)
+        self.set_email(email)
+        self.set_senha(senha)
 
-    @property 
-    def id(self) -> int:
-        return self._id 
+    def get_id(self) -> int:
+        return self.id 
 
-    @id.setter 
-    def id(self, novo_id: int) -> None:
-        self._id = novo_id
+    def set_id(self, novo_id: int) -> None:
+        self.id = novo_id
 
-    @property 
-    def tipo(self) -> TipoUsuario:
-        return self._tipo 
+    def get_tipo(self) -> TipoUsuario:
+        return self.tipo 
 
-    @tipo.setter 
-    def tipo(self, novo_tipo: TipoUsuario) -> None:
-        self._tipo = novo_tipo
+    def set_tipo(self, novo_tipo: TipoUsuario) -> None:
+        self.tipo = novo_tipo
 
-    @property 
-    def nome(self) -> str:
-        return self._nome 
+    def get_nome(self) -> str:
+        return self.nome 
     
-    @nome.setter 
-    def nome(self, novo_nome: str) -> None:
-        self._nome = novo_nome
+    def set_nome(self, novo_nome: str) -> None:
+        self.nome = novo_nome
 
-    @property 
-    def data_nascimento(self) -> datetime:
-        return datetime.fromtimestamp(self._data_nascimento.timestamp())
+    def get_data_nascimento(self) -> datetime:
+        return datetime.fromtimestamp(self.data_nascimento.timestamp())
 
-    @data_nascimento.setter
-    def data_nascimento(self, nova_data: datetime) -> None:
-        self._data_nascimento = datetime.fromtimestamp(nova_data.timestamp())
+    def set_data_nascimento(self, nova_data: datetime) -> None:
+        self.data_nascimento = datetime.fromtimestamp(nova_data.timestamp())
 
-    @property 
-    def email(self) -> str:
-        return self._email
+    def get_email(self) -> str:
+        return self.email
 
-    @email.setter 
-    def email(self, novo_email: str) -> None:
-        self._email = novo_email
+    def set_email(self, novo_email: str) -> None:
+        self.email = novo_email
 
-    @property 
-    def senha(self) -> str:
-        return self._senha
+    def get_senha(self) -> str:
+        return self.senha
 
-    @senha.setter 
-    def senha(self, nova_senha: str) -> None:
-        self._senha = nova_senha
+    def set_senha(self, nova_senha: str) -> None:
+        self.senha = nova_senha
