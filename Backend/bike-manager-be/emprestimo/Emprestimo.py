dac445c877e1c6d3ae6ee6aed65a8ae97a25647d
Tiago Ceccon <ceccon.t@hotmail.com>
2021-11-14 15:38:12 -0300

    BE - Altera entidades Emprestimo e Multa para nao usarem properties

--- Backend/bike-manager-be/emprestimo/Emprestimo.py
+++ Backend/bike-manager-be/emprestimo/Emprestimo.py
@@ -3,103 +3,81 @@ from datetime import datetime
 class Emprestimo:
 
     def __init__(self, id: int, id_adm: int, id_cliente: int, id_levador: int, id_buscador: int, id_bicicleta: int, inicio: datetime, fim: datetime, quilometragem: int, recebida: bool, paga: bool):
-        self.id = id,
-        self.id_adm = id_adm 
-        self.id_cliente = id_cliente 
-        self.id_levador = id_levador
-        self.id_buscador = id_buscador
-        self.id_bicicleta = id_bicicleta
-        self.inicio = datetime.fromtimestamp(inicio.timestamp())
-        self.fim = datetime.fromtimestamp(fim.timestamp())
-        self.quilometragem = quilometragem
-        self.recebida = recebida
-        self.paga = paga 
+        self.set_id(id)
+        self.set_id_adm(id_adm) 
+        self.set_id_cliente(id_cliente) 
+        self.set_id_levador(id_levador)
+        self.set_id_buscador(id_buscador)
+        self.set_id_bicicleta(id_bicicleta)
+        self.set_inicio(datetime.fromtimestamp(inicio.timestamp()))
+        self.set_fim(datetime.fromtimestamp(fim.timestamp()))
+        self.set_quilometragem(quilometragem)
+        self.set_recebida(recebida)
+        self.set_paga(paga) 
 
-    @property 
-    def id(self) -> int:
+    def get_id(self) -> int:
         return self.id 
 
-    @id.setter 
-    def id(self, novo_id: int) -> None:
+    def set_id(self, novo_id: int) -> None:
         self.id = novo_id
 
-    @property 
-    def id_adm(self) ->  int:
+    def get_id_adm(self) ->  int:
         return self.id_adm 
     
-    @id_adm.setter 
-    def id_adm(self, novo_id_adm: int) -> None:
+    def set_id_adm(self, novo_id_adm: int) -> None:
         self.id_adm = novo_id_adm
 
-    @property 
-    def id_cliente(self) -> int:
+    def get_id_cliente(self) -> int:
         return self.id_cliente
 
-    @id_cliente.setter 
-    def id_cliente(self, novo_id_cliente: int) -> None:
+    def set_id_cliente(self, novo_id_cliente: int) -> None:
         self.id_cliente = novo_id_cliente
 
-    @property 
-    def id_levador(self) -> int:
+    def get_id_levador(self) -> int:
         return self.id_levador
 
-    @id_levador.setter 
-    def id_levador(self, novo_id_levador: int) -> None:
+    def set_id_levador(self, novo_id_levador: int) -> None:
         self.id_levador = novo_id_levador
 
-    @property 
-    def id_buscador(self) -> int:
+    def get_id_buscador(self) -> int:
         return self.id_buscador
 
-    @id_buscador.setter 
-    def id_buscador(self, novo_id_buscador: int) -> None:
+    def set_id_buscador(self, novo_id_buscador: int) -> None:
         self.id_buscador = novo_id_buscador
 
-    @property 
-    def id_bicicleta(self) -> int:
+    def get_id_bicicleta(self) -> int:
         return self.id_bicicleta
 
-    @id_bicicleta.setter 
-    def id_bicicleta(self, novo_id_bicicleta: int) -> None:
+    def set_id_bicicleta(self, novo_id_bicicleta: int) -> None:
         self.id_bicicleta = novo_id_bicicleta
 
-    @property 
-    def inicio(self) -> datetime:
+    def get_inicio(self) -> datetime:
         return datetime.fromtimestamp(self.inicio.timestamp())
 
-    @inicio.setter 
-    def inicio(self, novo_inicio: datetime) -> None:
+    def set_inicio(self, novo_inicio: datetime) -> None:
         self.inicio = datetime.fromtimestamp(novo_inicio.timestamp())
 
-    @property 
-    def fim(self) -> datetime:
+    def get_fim(self) -> datetime:
         return datetime.fromtimestamp(self.fim.timestamp())
 
-    @fim.setter 
-    def fim(self, novo_fim: datetime) -> None:
+    def set_fim(self, novo_fim: datetime) -> None:
         self.fim = datetime.fromtimestamp(novo_fim.timestamp())
 
-    @property 
-    def quilometragem(self) -> int:
+    def get_quilometragem(self) -> int:
         return self.quilometragem
 
-    @quilometragem.setter
-    def quilometragem(self, nova_quilometragem: int) -> None:
+    def set_quilometragem(self, nova_quilometragem: int) -> None:
         self.quilometragem = nova_quilometragem
 
-    @property 
-    def recebida(self) -> bool:
+    def get_recebida(self) -> bool:
         return self.recebida
 
-    @recebida.setter 
-    def recebida(self, novo_recebida: bool) -> None:
+    def set_recebida(self, novo_recebida: bool) -> None:
         self.recebida = novo_recebida
 
-    @property 
-    def paga(self) -> bool:
+    def get_paga(self) -> bool:
         return self.paga
 
-    @paga.setter 
-    def paga(self, novo_paga: bool) -> None:
+    def set_paga(self, novo_paga: bool) -> None:
         self.paga = novo_paga
 
--- Backend/bike-manager-be/multa/Multa.py
+++ Backend/bike-manager-be/multa/Multa.py
@@ -2,30 +2,24 @@
 class Multa:
 
     def __init__(self, id: int, id_emprestimo: int, valor: int):
-        self.id = id 
-        self.id_emprestimo = id_emprestimo
-        self.valor = valor 
+        self.set_id(id) 
+        self.set_id_emprestimo(id_emprestimo)
+        self.set_valor(valor) 
 
-    @property 
-    def id(self) -> int:
+    def get_id(self) -> int:
         return self.id 
 
-    @id.setter 
-    def id(self, novo_id: int) -> None:
+    def set_id(self, novo_id: int) -> None:
         self.id = novo_id
 
-    @property 
-    def id_emprestimo(self) -> int:
+    def get_id_emprestimo(self) -> int:
         return self.id_emprestimo
 
-    @id_emprestimo.setter 
-    def id_emprestimo(self, novo_id_emprestimo: int) -> None:
+    def set_id_emprestimo(self, novo_id_emprestimo: int) -> None:
         self.id_emprestimo = novo_id_emprestimo
 
-    @property 
-    def valor(self) -> int:
+    def get_valor(self) -> int:
         return self.valor 
 
-    @valor.setter 
-    def valor(self, novo_valor) -> None:
+    def set_valor(self, novo_valor) -> None:
         self.valor = novo_valor
