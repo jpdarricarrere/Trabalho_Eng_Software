2b237a6d0a50f339a25808362917e5259038ef10
gdsantana <guilherme30santana@gmail.com>
2021-11-14 18:09:47 -0300

    FE - View de add bike

---
+++ Frontend/bike/src/views/CreateProduct.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="card ">
+        <div class="card-body">
+            <form>
+            <label class="form-label mt-2" >Nome da bicicleta</label>
+            <input type="text" class="form-control"  > 
+            <label class="form-label mt-2" >Descrição</label>
+            <input type="text" class="form-control"  > 
+            <label class="form-label mt-2" >Valor do aluguel</label>
+            <input type="text" class="form-control"  > 
+            
+            </form>
+        </div>
+        </div>
+
+    </div>
+</template>
