0e27628e786832fdbf87841c5d2495f63dbcdbe0
Raphael Malinski <raphaelmalinski@gmail.com>
2022-03-26 16:04:44 -0300

    Refatorado navbar

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -1,33 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <ul class="navbar-nav">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/logo-bike-nome.png" height="75" alt="Logo-rike" />
-      </a>
-    </ul>
-
-    <!--<ul class="navbar-nav">
-     <form class="text">
-        <input
-          class="form-control"
-          type="text"
-          placeholder="Pesquisar pelo nome ou modelo..."
-          v-model="search" @keyup.enter="submit"
-        />       
-      </form>
-
-    </ul>-->
-    <ul class="navbar-nav">
-      <li class="nav-item me-3">
-        <router-link to="/home" active-class="active" class="nav-link" exact>
-          <a class="nav-link text-primary fw-bolder" href="#">
-            <b-icon-house-fill></b-icon-house-fill>
-            <i>Home</i>
-          </a>
-        </router-link>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="navbar-brand">
+        <img src="../assets/logo-social-sq.png" alt="Logo do site" height="60"/>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="padding-top: 25px">Home</a>
+      </li>
+      <li class="nav-item text-right">
+        <form class="form-inline my-2 my-lg-0" style="padding-top: 20px">
+          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar por serviço" v-model="search" @keyup.enter="submit">
+        </form>
+    </li>
     </ul>
-  </nav>
+  </div>
+</nav>
 </template>
 <script>
 export default {
